c59bcd5dcd20143ec317a3e9c319ad467b04a199
kyrea <notaeryk@gmail.com>
2023-08-09 20:51:42 +0530

    Added a cool beta checker which won't burn your eyes

--- src/pages/not-beta-tester.js
+++ src/pages/not-beta-tester.js
@@ -1,13 +1,32 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const NotBetaTester = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <div className="max-w-md p-8 mx-auto bg-white rounded-lg shadow-lg">
-        <h1 className="text-2xl font-semibold mb-4">You are not a beta tester</h1>
-        <p className="text-gray-700 mb-6">Sorry, you do not have the required role to access this page.</p>
-        <Link href="/">
-          <a className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300">Home</a>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-700">
+      <div className="max-w-md p-8 mx-auto bg-gray-800 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-semibold mb-4 text-white">
+          You are not a Beta Tester
+        </h1>
+        <p className="text-gray-300 mb-6">
+          Sorry, you do not have the
+          <span className="inline-block ml-1">
+            <span
+              className="text-white px-2 py-1 rounded-lg"
+              style={{
+                color: "rgb(0, 244, 85)",
+                backgroundColor: "rgba(0, 244, 85, 0.1)",
+              }}
+            >
+              <span>@🧪 Beta Tester</span>
+            </span>
+          </span>
+          &nbsp;role to access this feature.
+        </p>
+        <Link
+          href="/"
+          className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+        >
+          Home{" "}
         </Link>
       </div>
     </div>
