4e64ae3d60ed7f21f9bd5b16da1b302663f23733
kyrea <notaeryk@gmail.com>
2024-02-14 01:56:36 +0530

    Added a adsense meta tag (no version update)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,16 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-1733730047414795" />
+      </Head>
       <Component {...pageProps} />
       <ReactQueryDevtools />
     </QueryClientProvider>
