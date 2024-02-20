0770eb62f5e057451efab88f9eee0ed3e9a8d74e
kyrea <notaeryk@gmail.com>
2024-02-20 21:05:55 +0530

    Added automatic ads

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,6 +12,11 @@ function MyApp({ Component, pageProps }) {
     <QueryClientProvider client={queryClient}>
       <Head>
         <meta name="google-adsense-account" content="ca-pub-1733730047414795" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1733730047414795"
+          crossorigin="anonymous"
+        ></script>
       </Head>
       <Component {...pageProps} />
       <ReactQueryDevtools />
