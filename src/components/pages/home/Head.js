bb189a404806b81c35374f460ecdddd60506e75f
kyrea <notaeryk@gmail.com>
2025-02-23 19:46:00 +0530

    refactor(ui): moved the Head component of the home page for better readability

---
+++ src/components/pages/home/Head.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+export default function HomeHead() {
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content="Aeryk" />
+      <title>Waifu.it - Home</title>
+      <meta
+        name="description"
+        content="Discover a user-friendly open-source API designed to access a vast collection of 80,000+ anime waifu data and images, alongside a wide range of quotes, facts, and additional endpoints."
+      />
+      <meta
+        name="keywords"
+        content="anime, waifu, anime facts, anime quotes, waifu api, waifu pictures, anime api, open source api"
+      />
+      <meta property="og:title" content="Waifu.it" />
+      <meta property="og:url" content="https://waifu.it" />
+      <meta
+        property="og:image"
+        content="https://avatars.githubusercontent.com/u/79479798?s=200&v=4"
+      />
+      <meta
+        property="og:description"
+        content="Discover a user-friendly open-source API designed to access a vast collection of 80,000+ anime waifu data and images, alongside a wide range of quotes, facts, and additional endpoints."
+      />
+      <meta name="theme-color" content="#1DA1F2" />
+      <meta
+        name="google-adsense-account"
+        content="ca-pub-1733730047414795"
+      ></meta>
+    </Head>
+  );
+}
