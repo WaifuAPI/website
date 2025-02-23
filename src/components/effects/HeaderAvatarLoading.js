0ed3c76904bff622d36931e5f04308e7d3c130cd
kyrea <notaeryk@gmail.com>
2025-02-23 19:17:08 +0530

    feat(ui): add skeleton loaders for HeaderAvatar and ProfileCard

---
+++ src/components/effects/HeaderAvatarLoading.js
@@ -0,0 +1,13 @@
+export default function HeaderAvatarLoading() {
+  return (
+    <div className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-600 bg-gray-500 cursor-pointer animate-pulse">
+      {/* <Image
+                src="https://cdn.discordapp.com/avatars/649452442253000704/be23b6127272c634c577a0f3551bc115.webp" // Sample avatar
+                alt="User Avatar"
+                width={100} // Specify width (adjust as needed)
+                height={100} // Specify height (adjust as needed)
+                className="w-full h-full object-cover"
+              /> */}
+    </div>
+  );
+}
