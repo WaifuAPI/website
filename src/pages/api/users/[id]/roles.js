25995fdafb664140844e8f9ae9b52f762bbdf91a
kyrea <notaeryk@gmail.com>
2025-02-26 15:47:12 +0530

    feat(api): added GET users/:id/roles to get user roles from external api

---
+++ src/pages/api/users/[id]/roles.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  if (!id) {
+    return res.status(400).json({ error: "User ID is required" });
+  }
+
+  try {
+    const response = await axios.get(
+      `http://localhost:4000/api/v4/user/profile/${id}`,
+      {
+        headers: {
+          key: process.env.ACCESS_KEY,
+        },
+      }
+    );
+
+    return res.status(200).json({ roles: response.data.roles });
+  } catch (error) {
+    console.log(error);
+    console.log(error.response?.data);
+    return res.status(error.response?.status || 500).json({
+      error: error.response?.data?.message || "Internal Server Error",
+    });
+  }
+}
